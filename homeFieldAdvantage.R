a1bb84ea7a677856269b669461ea9bac007ff46a
ben-rishel <ben.rishel@icloud.com>
2025-04-29 14:22:56 -0400

    Running T-test for Home Field Advantage
    Gathering data from every match in PL 2025 season to determine statistical significance of home field advantage

--- homeFieldAdvantage.R
+++ homeFieldAdvantage.R
@@ -15,12 +15,14 @@
 # Repeat for other leagues
 
 ### Import Packages
-
 library(worldfootballR)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(knitr)
 
+
+### Load in Data
 pl_team_stats_raw <- fb_season_team_stats(
   country = "ENG", 
   gender = "M", 
@@ -29,22 +31,22 @@ pl_team_stats_raw <- fb_season_team_stats(
   stat_type = "league_table_home_away"
 )
 
-#glimpse(pl_team_stats_raw)
+### Data Wrangling
 
+## Selecting attributes for analysis, and making them numeric values
 pl_pts_per_match <- pl_team_stats_raw %>%
   select(
     Squad,     
-    MP_Home,
-    MP_Away,
     Pts_per_MP_Home,
     Pts_per_MP_Away,
     xGD_per_90_Home,
     xGD_per_90_Away
   ) %>%
-  mutate(
+  mutate( #Force stats to be numeric
     across(c(Pts_per_MP_Home, Pts_per_MP_Away, xGD_per_90_Home, xGD_per_90_Away), as.numeric)
   )
 
+## Creating Summary Table with Points Gained Metrics
 pl_more_pts_home <- pl_pts_per_match %>%
   summarize(
     Squad = Squad,
@@ -59,7 +61,7 @@ ggplot(
     x = Pts_gained_at_home_per_match
     )
   ) +
-  geom_density(
+  geom_density( #density plot
     fill = "#69b3a2", 
     color = "#e9ecef"
   ) + 
@@ -69,7 +71,7 @@ ggplot(
     title = "Premier League Points Gained at Home"
   ) +
   scale_x_continuous(
-    limits = c(-.75, 1.1)
+    limits = c(-.75, 1.1) #Making sure all data fits by setting x bounds
     ) + 
   theme_light()
 
@@ -84,8 +86,8 @@ ggplot(
   geom_density(
     aes(y = ..density..),
     fill = "#69b3a2"
-  ) +
-  geom_label(
+  ) + 
+  geom_label( #adds label to top chart
     aes(x = .8, y = 1, label = "Points Gained at Home Per Match")
   ) +
   # Bottom (flipped)
@@ -93,7 +95,7 @@ ggplot(
     aes(x = xPts_gained_at_home_per_match, y = -..density..),
     fill = "#404080"
   ) +
-  geom_label(
+  geom_label( #adds label to bottom chart
     aes(x = .8, y = -1, label = "xPoints Gained at Home per Match")
   ) + 
   labs(
@@ -102,3 +104,60 @@ ggplot(
     title = "Premier League Points/Expected Points Gained at Home"
   ) +
   theme_light()
+
+
+#Testing League-Wide Home Field Advantage Statistical Significance
+
+pl_2025_match_raw <- fb_match_results(country = "ENG", gender = "M", season_end_year = 2025, tier = "1st")
+
+## Selecting attributes for analysis, and making them numeric values
+pl_2025_match <- pl_2025_match_raw %>%
+  select(
+    HomeGoals,     
+    AwayGoals
+  ) %>%
+  mutate( #Force stats to be numeric
+    across(c(HomeGoals, AwayGoals), as.numeric)
+  )
+
+## Creating Summary Table with Match as Case, attributes homePts, awayPts
+pl_homeAwayPts <- pl_2025_match %>%
+  mutate(
+    homePts = case_when( ## When home scores more, win, if same, tie
+      HomeGoals  > AwayGoals ~ 3,
+      HomeGoals  == AwayGoals ~ 1,
+      HomeGoals  < AwayGoals ~ 0
+    ),
+    awayPts = case_when(
+      AwayGoals  > HomeGoals ~ 3,
+      AwayGoals  == HomeGoals ~ 1,
+      AwayGoals < HomeGoals ~ 0
+    )
+  )
+
+## Creating table to be used for t-test
+pl_homePtsGained <- pl_homeAwayPts %>%
+  summarize(
+    homePtsGained = homePts - awayPts
+  ) 
+
+test < - t.test( 
+        pl_homePtsGained$homePtsGained,
+        mu = 0,          # Null Hypothesis: no home advantage
+        alternative = "greater" ) #One sided t-test
+
+## Building Dataframe with info from t-test
+test_info <- data.frame(
+  `t statistic` = unname(test$statistic),
+  df            = unname(test$parameter),
+  `p value`     = test$p.value,
+  `CI lower`    = test$conf.int[1],
+  `CI upper`    = test$conf.int[2],
+  check.names   = FALSE  # keep our nice column names
+)
+
+## Create Nice Table with Kable
+test_info %>% kable(
+      digits  = 3,
+      caption = "One-Sample T-Test: Home vs. Away Point Difference"
+      )
